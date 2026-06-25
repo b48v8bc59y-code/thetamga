6b606015ff9ece1d2e0cad141dc751119f74d489
b48v8bc59y-code <b48v8bc59y@privaterelay.appleid.com>
2026-06-25 23:15:33 +0500

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,6 +1,6 @@
-import crypto from 'crypto';
+const crypto = require('crypto');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
@@ -46,5 +46,6 @@ export default async function handler(req, res) {
 
   const data = await response.json();
   return res.status(200).json({ ok: true, data });
-}
+};
+
 
